eb09edbce5255091ceb46d60af6f32fe6a0c9b9b
Albin K P <albin.kp@nestgroup.net>
2026-08-21 16:27:02 +0530

    results deletion fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4186673708" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "943169695" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
